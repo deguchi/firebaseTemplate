16b28e9f09fb1a091a1fbd78a973b324e70d0a72
deguchi <deguchik@gmail.com>
2021-04-24 11:02:09 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
+import firebaseConfig from '../firebaseConfig.json'
+
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 if (location.hostname === 'localhost' || location.hostname.match(/192\.168\.0\.\w+$/)) {
